53bc40b98c13a83dc5b0926d653d5c83e04acd9a
ShizzaHo <chekan898@gmail.com>
2023-08-22 13:36:20 +0500

    minor improvements, adaptation to new Helium functions

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { startApp } from "Helium/web/index.js";
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-// return __dirname to es6
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-// run Helium application
-startApp(3000, __dirname);
---
+++ src/startup.js
@@ -0,0 +1,12 @@
+import { startApp } from "Helium/web/index.js";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+// return __dirname to es6
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// run Helium application
+startApp(3000, __dirname, {
+    spa: true, // Single Page Application, it is recommended to use without using third-party libraries such as React
+    useBuild: true, // Each index.js file will be compiled via esbuild, allowing you to create more complex applications
+});
